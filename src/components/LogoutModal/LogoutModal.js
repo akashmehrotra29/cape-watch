5c606b8ef78405571707b89db9e9a501c515db2b
akashmehrotra29 <akashmehrotra29@gmail.com>
2021-07-15 13:33:22 +0530

    fix: logout modal

--- src/components/LogoutModal/LogoutModal.js
+++ src/components/LogoutModal/LogoutModal.js
@@ -7,25 +7,26 @@ export const LogoutModal = ({ setShowLogoutModal }) => {
 
   return (
     <div className={`${styles.modalOuter}`}>
-      <div className={`${styles.modalInner}`}></div>
-      <h2>Are you sure you want to logout</h2>
-      <div className={`${styles.modalButtonWrap}`}>
-        <button
-          className="btn btn-outlined btn-small"
-          onClick={() => setShowLogoutModal(false)}
-        >
-          Cancel
-        </button>
-        <button
-          className="btn btn-solid btn-small"
-          onClick={() => {
-            logoutUser();
-            dispatch({ type: "RESET_PLAYLISTS" });
-            setShowLogoutModal(false);
-          }}
-        >
-          Logout
-        </button>
+      <div className={`${styles.modalInner}`}>
+        <h2>Are you sure you want to logout</h2>
+        <div className={`${styles.modalButtonWrap}`}>
+          <button
+            className="btn btn-secondary"
+            onClick={() => setShowLogoutModal(false)}
+          >
+            Cancel
+          </button>
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              logoutUser();
+              dispatch({ type: "RESET_PLAYLISTS" });
+              setShowLogoutModal(false);
+            }}
+          >
+            Logout
+          </button>
+        </div>
       </div>
     </div>
   );
